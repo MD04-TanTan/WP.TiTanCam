aa9d5f4dcd6b8670ff6ecd964414bf636c9366dd
Tan Tan <md04_tantt@vtca.edu.vn>
2015-03-27 21:12:41 +0700

    27/3 Tân commit
    Update

---
+++ ViewModel/ResolutionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP.TiTanCam.ViewModel
+{
+    public enum Resolution { WVGA, WXGA, HD };
+
+    public class ResolutionHelper
+    {
+        private static bool IsWvga
+        {
+            get { return App.Current.Host.Content.ScaleFactor == 100; }
+        }
+
+        private static bool IsWxga
+        {
+            get { return App.Current.Host.Content.ScaleFactor == 160; }
+        }
+
+        private static bool IsHD
+        {
+            get { return App.Current.Host.Content.ScaleFactor == 150; }
+        }
+
+        public static Resolution CurrentResolution
+        {
+            get
+            {
+                if (IsWvga)return Resolution.WVGA;
+                else if (IsWxga) return Resolution.WXGA;
+                else if (IsHD) return Resolution.HD;
+                else throw new InvalidOperationException("Unknows resolution");
+            }
+        }
+    }
+}
